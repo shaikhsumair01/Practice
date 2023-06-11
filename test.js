801cd90ec16b33465571df3f6c2f017b524fa75a
shaikhsumair01 <124897639+shaikhsumair01@users.noreply.github.com>
2023-06-11 21:55:12 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+// Implementing the mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
